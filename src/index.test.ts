21c7a5302c159feb8f92232be716690c94f94e4b
Simon Knott <info@simonknott.de>
2020-08-04 10:32:08 +0200

    Create test with expectation for plugin

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,13 +5,36 @@ const dontUsePrettier = (c: string) => c
 
 pluginTester({
   plugin: superJsonWithNext,
-   
+
   tests: {
     'changes this code': {
       // input to the plugin
-      code: 'var hello = "hi";',
+      code: `
+export const getServerSideProps = async () => {
+  const products = [{ name: 'Hat', publishedAt: new Date(0) }];
+
+  return { props: { products } };
+};
+
+export default function Page({ products }) {
+  return JSON.stringify(products)
+}
+      `,
       // expected output
-      output: 'var olleh = "hi";',
+      output: `
+import SuperJSON from "superjson";
+
+export const getServerSideProps = async () => {
+  const products = [{ name: 'Hat', publishedAt: new Date(0) }];
+
+  return { props: { SuperJSON: SuperJSON.serialize({ products }) } };
+};
+
+export default function Page(props) {
+  let { products } = SuperJSON.deserialize(props.SuperJSON);
+  return JSON.stringify(products)
+}
+      `,
     },
   },
 
