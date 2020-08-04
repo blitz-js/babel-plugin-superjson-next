c04b940352cceb5ad1d6595418cc9018873deb3b
Simon Knott <info@simonknott.de>
2020-08-04 14:40:26 +0200

    Add support for class components

--- src/index.test.ts
+++ src/index.test.ts
@@ -50,6 +50,55 @@ export default function Page(props) {
       `,
     },
 
+    'transforms a valid example using class components': {
+      // input to the plugin
+      code: `
+export const getServerSideProps = async () => {
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0)
+    }
+  ];
+
+  return { props: { products } };
+};
+
+export default class Page {
+  render({ products }) {
+    return JSON.stringify(products)
+  }
+}
+      `,
+      output: `
+import SuperJSON from "superjson";
+
+export const getServerSideProps = async () => {
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0),
+    },
+  ];
+
+  return ((r) => (r.props ? ((r.props = SuperJSON.serialize(r.props)), r) : r))(
+    {
+      props: {
+        products,
+      },
+    }
+  );
+};
+
+export default class Page {
+  render(props) {
+    let { products } = SuperJSON.deserialize(props.SuperJSON);
+    return JSON.stringify(products);
+  }
+}
+      `,
+    },
+
     "does not change a page that doesn't export gSSP": `
 const getServerSideProps = async () => {
   const products = [
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { PluginObj, types } from '@babel/core';
+import { PluginObj, types, NodePath } from '@babel/core';
 
 function transformReturnValue(value: types.Expression): types.Expression {
   const returnValueIdentifier = types.identifier('r');
@@ -31,30 +31,88 @@ function transformReturnValue(value: types.Expression): types.Expression {
   ]);
 }
 
+function transformPageFunctionComponent<
+  T extends types.FunctionDeclaration | types.ClassMethod
+>(func: T): T {
+  if (func.params.length === 0) {
+    return func;
+  }
+
+  const newPropsParamIdentifier = types.identifier('props');
+
+  const {
+    params,
+    body: { body },
+  } = func;
+
+  const [oldPropsParam] = params;
+  params[0] = newPropsParamIdentifier;
+
+  const deserializationLine = types.variableDeclaration('let', [
+    types.variableDeclarator(
+      oldPropsParam,
+      types.callExpression(types.identifier('SuperJSON.deserialize'), [
+        types.memberExpression(newPropsParamIdentifier, types.identifier('SuperJSON')),
+      ])
+    ),
+  ]);
+
+  body.unshift(deserializationLine);
+
+  return func;
+}
+
+function transformPageClassComponent(
+  component: types.ClassDeclaration
+): types.ClassDeclaration {
+  function isRenderFunction(decl: typeof component.body.body[0]): boolean {
+    return (
+      types.isClassMethod(decl) &&
+      types.isIdentifier(decl.key) &&
+      decl.key.name === 'render'
+    );
+  }
+
+  component.body.body = component.body.body.map(decl => {
+    if (isRenderFunction(decl)) {
+      return transformPageFunctionComponent(decl as types.ClassMethod);
+    }
+
+    return decl;
+  });
+
+  return component;
+}
+
+function isGetServerSidePropsDeclaration(
+  path: NodePath<types.ExportNamedDeclaration>
+): boolean {
+  const { declaration } = path.node;
+
+  if (!types.isVariableDeclaration(declaration)) {
+    return false;
+  }
+
+  const [{ id, init }] = declaration.declarations;
+  if (!types.isIdentifier(id)) {
+    return false;
+  }
+
+  if (id.name !== 'getServerSideProps') {
+    return false;
+  }
+
+  return (
+    types.isArrowFunctionExpression(init) || types.isFunctionExpression(init)
+  );
+}
+
 function superJsonWithNext(): PluginObj {
   return {
     name: 'replace gSSP',
     visitor: {
       ExportNamedDeclaration(path) {
-        const { declaration } = path.node;
-
-        if (!types.isVariableDeclaration(declaration)) {
-          return;
-        }
-
-        const [{ id, init }] = declaration.declarations;
-        if (!types.isIdentifier(id)) {
-          return;
-        }
-
-        if (id.name !== 'getServerSideProps') {
-          return;
-        }
-
-        if (
-          types.isArrowFunctionExpression(init) ||
-          types.isFunctionExpression(init)
-        ) {
+        if (isGetServerSidePropsDeclaration(path)) {
           path.traverse({
             ReturnStatement(path) {
               if (path.node.argument) {
@@ -67,30 +125,13 @@ function superJsonWithNext(): PluginObj {
       ExportDefaultDeclaration(path) {
         const { declaration } = path.node;
 
-        if (!types.isFunctionDeclaration(declaration)) {
-          return;
+        if (types.isFunctionDeclaration(declaration)) {
+          path.node.declaration = transformPageFunctionComponent(declaration);
         }
 
-        if (declaration.params.length === 0) {
-          return;
+        if (types.isClassDeclaration(declaration)) {
+          path.node.declaration = transformPageClassComponent(declaration);
         }
-
-        const [ propsParam ] = declaration.params
-
-        const props = types.identifier("props")
-
-        const deserialization = types.variableDeclaration("let", [
-          types.variableDeclarator(
-            propsParam,
-            types.callExpression(types.identifier('SuperJSON.deserialize'), [
-              types.memberExpression(props, types.identifier("SuperJSON")),
-            ])
-          )
-        ])
-
-        declaration.body.body.unshift(deserialization)
-
-        declaration.params[0] = props
       },
     },
   };
