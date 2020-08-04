53894eb551afe481c3accb2f712003931601418c
Simon Knott <info@simonknott.de>
2020-08-04 14:12:22 +0200

    Create basic first go on plugin

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,20 @@
 import superJsonWithNext from './';
 import pluginTester from 'babel-plugin-tester';
 
-const dontUsePrettier = (c: string) => c
-
 pluginTester({
   plugin: superJsonWithNext,
 
   tests: {
-    'changes this code': {
+    'transforms a valid example': {
       // input to the plugin
       code: `
 export const getServerSideProps = async () => {
-  const products = [{ name: 'Hat', publishedAt: new Date(0) }];
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0)
+    }
+  ];
 
   return { props: { products } };
 };
@@ -20,23 +23,51 @@ export default function Page({ products }) {
   return JSON.stringify(products)
 }
       `,
-      // expected output
       output: `
 import SuperJSON from "superjson";
 
 export const getServerSideProps = async () => {
-  const products = [{ name: 'Hat', publishedAt: new Date(0) }];
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0),
+    },
+  ];
 
-  return { props: { SuperJSON: SuperJSON.serialize({ products }) } };
+  return ((r) => (r.props ? ((r.props = SuperJSON.serialize(r.props)), r) : r))(
+    {
+      props: {
+        products,
+      },
+    }
+  );
 };
 
 export default function Page(props) {
   let { products } = SuperJSON.deserialize(props.SuperJSON);
-  return JSON.stringify(products)
+  return JSON.stringify(products);
 }
       `,
     },
-  },
 
-  formatResult: dontUsePrettier,
+    "does not change a page that doesn't export gSSP": `
+const getServerSideProps = async () => {
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0),
+    },
+  ];
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+export default function Page({ products }) {
+  return JSON.stringify(products);
+}
+    `,
+  },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,96 @@
-import { PluginObj } from '@babel/core';
+import { PluginObj, types } from '@babel/core';
 
-function reverse(string: string): string {
-  return string
-    .split('')
-    .reverse()
-    .join('');
+function transformReturnValue(value: types.Expression): types.Expression {
+  const returnValueIdentifier = types.identifier('r');
+  const propsOfR = types.memberExpression(
+    returnValueIdentifier,
+    types.identifier('props')
+  );
+  const body = types.conditionalExpression(
+    propsOfR,
+    types.sequenceExpression([
+      types.assignmentExpression(
+        '=',
+        propsOfR,
+        types.callExpression(types.identifier('SuperJSON.serialize'), [
+          propsOfR,
+        ])
+      ),
+      returnValueIdentifier,
+    ]),
+    returnValueIdentifier
+  );
+
+  const creatorFunction = types.arrowFunctionExpression(
+    [returnValueIdentifier],
+    body
+  );
+
+  return types.callExpression(types.parenthesizedExpression(creatorFunction), [
+    value,
+  ]);
 }
 
 function superJsonWithNext(): PluginObj {
   return {
-    name: 'identifier reverse',
+    name: 'replace gSSP',
     visitor: {
-      Identifier(idPath) {
-        idPath.node.name = reverse(idPath.node.name);
+      ExportNamedDeclaration(path) {
+        const { declaration } = path.node;
+
+        if (!types.isVariableDeclaration(declaration)) {
+          return;
+        }
+
+        const [{ id, init }] = declaration.declarations;
+        if (!types.isIdentifier(id)) {
+          return;
+        }
+
+        if (id.name !== 'getServerSideProps') {
+          return;
+        }
+
+        if (
+          types.isArrowFunctionExpression(init) ||
+          types.isFunctionExpression(init)
+        ) {
+          path.traverse({
+            ReturnStatement(path) {
+              if (path.node.argument) {
+                path.node.argument = transformReturnValue(path.node.argument);
+              }
+            },
+          });
+        }
+      },
+      ExportDefaultDeclaration(path) {
+        const { declaration } = path.node;
+
+        if (!types.isFunctionDeclaration(declaration)) {
+          return;
+        }
+
+        if (declaration.params.length === 0) {
+          return;
+        }
+
+        const [ propsParam ] = declaration.params
+
+        const props = types.identifier("props")
+
+        const deserialization = types.variableDeclaration("let", [
+          types.variableDeclarator(
+            propsParam,
+            types.callExpression(types.identifier('SuperJSON.deserialize'), [
+              types.memberExpression(props, types.identifier("SuperJSON")),
+            ])
+          )
+        ])
+
+        declaration.body.body.unshift(deserialization)
+
+        declaration.params[0] = props
       },
     },
   };
