cf054c1117863006854818eeab499bbc741451e3
Simon Knott <info@simonknott.de>
2020-08-09 08:43:49 +0200

    Fix integration tests

--- example/tests/index.ts
+++ example/tests/index.ts
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 fixture`Index`.page`http://localhost:3000/`;
 
-test('Index', async (t) => {
+test('Date is preserved', async (t) => {
   const result = Selector('#__next');
-  await t.expect(result.value).eql('props.date is Date: true');
+  await t.expect(result.innerText).eql('props.date is Date: true');
 });
--- example/tests/static.ts
+++ example/tests/static.ts
@@ -1,8 +1,8 @@
 import { Selector } from 'testcafe';
 
-fixture`Index`.page`http://localhost:3000/static`;
+fixture`Static`.page`http://localhost:3000/static`;
 
-test('Index', async (t) => {
+test('Date is preserved', async (t) => {
   const result = Selector('#__next');
-  await t.expect(result.value).eql('props.date is Date: true');
+  await t.expect(result.innerText).eql('props.date is Date: true');
 });
