ca2aee8f31291d434974c9c0f076be1e46037af8
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2020-09-28 22:04:05 +0200

    Prevents loss of static methods

--- src/tools.tsx
+++ src/tools.tsx
@@ -1,4 +1,5 @@
 import SuperJSON from 'superjson';
+import * as hoistNonReactStatics from 'hoist-non-react-statics';
 import type { GetServerSideProps } from 'next';
 import * as React from 'react';
 
@@ -10,13 +11,13 @@ export function withSuperJSONProps<P>(
   return async function withSuperJSON(...args) {
     const result = await gssp(...args);
     const { json, meta } = SuperJSON.serialize(result.props as any)
-    
+
     const props: SuperJSONResult = { json }
 
     if (Boolean(meta)) {
       props.meta = meta
     }
-    
+
     return {
       ...result,
       props
@@ -27,8 +28,12 @@ export function withSuperJSONProps<P>(
 export function withSuperJSONPage<P>(
   Page: React.ComponentType<P>
 ): React.ComponentType<SuperJSONResult> {
-  return function WithSuperJSON(serializedProps: any) {
+  const WithSuperJSON = (serializedProps: any) => {
     const props = (SuperJSON.deserialize(serializedProps) as unknown) as P;
     return <Page {...props} />;
   };
+
+  hoistNonReactStatics(WithSuperJSON, Page);
+
+  return WithSuperJSON;
 }
