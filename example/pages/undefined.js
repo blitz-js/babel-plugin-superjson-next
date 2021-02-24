edd28a028e10df4f29c1bee90581f1dd92e1c06d
Simon Knott <info@simonknott.de>
2021-02-24 08:41:52 +0100

    Remove unfulfillable test
    (would have errored during build)

--- example/pages/undefined.js
+++
@@ -1,9 +0,0 @@
-export async function getStaticProps() {
-  return {
-    props: undefined,
-  };
-}
-
-export default function Page(props) {
-  return 'props: ' + JSON.stringify(props);
-}
--- example/tests/undefined.ts
+++
@@ -1,8 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture`Null`.page`http://localhost:3099/undefined`;
-
-test('Raw object is built', async (t) => {
-  const result = Selector('#__next');
-  await t.expect(result.innerText).eql('props: {}');
-});
