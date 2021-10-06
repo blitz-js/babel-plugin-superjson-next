020d7c4bd2f2f90de45647e06645facc170f748d
Simon Knott <info@simonknott.de>
2021-10-06 09:15:35 +0000

    test go green

--- test/pages/export default from (repro #100)/output.js
+++ test/pages/export default from (repro #100)/output.js
@@ -1,8 +1,6 @@
-import { ssrUserPage } from 'lib/ssr_user_page';
 import { withSuperJSONPage as _withSuperJSONPage } from 'babel-plugin-superjson-next/tools';
 import { withSuperJSONProps as _withSuperJSONProps } from 'babel-plugin-superjson-next/tools';
-
-export const getServerSideProps = _withSuperJSONProps(ssrUserPage);
-
-import __superjsonDefaultImport from './user';
-export default _withSuperJSONPage(__superjsonDefaultImport);
+import { ssrUserPage } from 'lib/ssr_user_page';
+export const getServerSideProps = _withSuperJSONProps(ssrUserPage, ['smth']);
+import __superjsonLocalExport from './user';
+export default _withSuperJSONPage(__superjsonLocalExport);
