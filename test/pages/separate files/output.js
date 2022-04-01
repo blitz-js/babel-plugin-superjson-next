957032683140cdc9ade089ee20056e1a19e34cb5
Alistair Smith <hi@alistair.sh>
2022-04-01 11:25:52 +0100

    feat: implement failing test case

---
+++ test/pages/separate files/output.js
@@ -0,0 +1,10 @@
+import { withSuperJSONPage as _withSuperJSONPage } from 'babel-plugin-superjson-next/tools';
+import { withSuperJSONProps as _withSuperJSONProps } from 'babel-plugin-superjson-next/tools';
+import { getStaticProps } from './props';
+
+function Page({ products }) {
+  return JSON.stringify(products);
+}
+
+export const getStaticProps = _withSuperJSONProps(getStaticProps);
+export default _withSuperJSONPage(Page);
---
+++ test/pages/separate files/props.js
@@ -0,0 +1,14 @@
+export const getStaticProps = async () => {
+  const products = [
+    {
+      name: 'Hat',
+      publishedAt: new Date(0),
+    },
+  ];
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
